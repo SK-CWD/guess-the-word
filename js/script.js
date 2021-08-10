f8ca56a5f964705f8edfe8a1470d113b78fcc58d
SK-CWD <85139957+SK-CWD@users.noreply.github.com>
2021-08-10 11:46:45 -0500

    Created stipulations for using only letters in the game

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ const amountRemaining = document.querySelector(".remaining span");
 const message = document.querySelector(".message");
 const hiddenButton = document.querySelector(".play-again");
 const word = "magnolia";
+const guessedLetters = [];
 
 //this function below will update the <p> with however many dots are needed to replace the "word" the user is guessing.
 const dots = function (word) {
@@ -22,7 +23,35 @@ dots(word);//dots will appear holding place for "MAGNOLIA". 8 dots will appear.
 
 button.addEventListener("click", function (e) {
     e.preventDefault();//prevents the default behavior of clicking a button, the formsubmitting, and reloading the page.
+    message.innerText = "";
     const guess = letter.value;
-    console.log(guess);
+    const goodGuess = validate(guess);
+
+    if (goodGuess) {
+        makeGuess(guess);
+    }
     letter.value=""; //empty the value of the input letter
 });
+
+const validate = function (input) {
+    const acceptedLetter = /[a-zA-Z]/;
+    if(input.length === 0 ){
+        message.innerText = "Please enter a letter from a-z.";
+    } else if (input.length > 1){
+        message.innerText = "Please enter only 1 letter at a time";
+    } else if (!input.match(acceptedLetter)) {
+        message.innerText = "Please enter letters only from A-Z";
+    } else {
+        return input;
+    }
+};
+
+const makeGuess = function (guess) {
+    guess = guess.toUpperCase();
+    if (guessedLetters.includes(guess)) {
+        message.innerText = "You already guessed that letter! Try again.";
+    } else {
+        guessedLetters.push(guess);
+        console.log(guessedLetters);
+    }
+};
